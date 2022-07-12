6a031929eb7e68aeb00e47903314db4e406cbefe
Aleksandr Butukhanov <avbutukhanov@gmail.com>
2022-07-12 18:10:22 +0800

    test to test the method getMostPlayer

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -10,8 +10,25 @@ public class GameStoreTest {
 
         GameStore store = new GameStore();
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = store.publishGame("Нетология Баттл Онлайн 2", "Аркады 2");
+        boolean containsGame = store.containsGame(game);
 
-        assertTrue(store.containsGame(game));
+        assertTrue(store.containsGame(game1));
+    }
+
+    @Test
+    public void shouldPlayTime() {
+
+        GameStore store = new GameStore();
+        Player player = new Player("Petya");
+
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = store.publishGame("Нетология Баттл Онлайн2", "Аркады2");
+        store.addPlayTime("Petya", 1);
+        store.addPlayTime("Vasya", 1);
+        store.addPlayTime("Anna", 1);
+        store.getMostPlayer();
+
     }
 
     // другие ваши тесты
